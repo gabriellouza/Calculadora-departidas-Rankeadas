323a60afd2190413bd7e43dc6254865031a2e00b
user <projetointegraai@gmail.com>
2025-03-13 11:59:35 -0300

    Calculadora de Partidas Rankeadas

---
+++ ranked.js
@@ -0,0 +1,28 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem um saldo de ${saldoVitorias} vitórias e está no nível de ${nivel}`;
+}
+
+let vitorias = 75;
+let derrotas = 30;
+let resultado = calcularNivel(vitorias, derrotas);
+
+console.log(resultado);
